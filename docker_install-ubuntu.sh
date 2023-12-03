8fc7f4fabe3c1b38b010e5d741dbaade83f51cf6
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-12-03 11:14:52 +0700

    fix: remove sudo and add #!/bin/bash

--- docker_install-ubuntu.sh
+++ docker_install-ubuntu.sh
@@ -1,20 +1,22 @@
-# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
-sudo apt update;
-sudo apt install ca-certificates curl gnupg -y;
+#!/bin/bash
 
-# Add Docker’s official GPG key:
-sudo install -m 0755 -d /etc/apt/keyrings;
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
-sudo chmod a+r /etc/apt/keyrings/docker.gpg;
+# ## Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+apt update;
+apt install ca-certificates curl gnupg -y;
 
-# Use the following command to set up the repository:
-sudo echo \
+# ## Add Docker’s official GPG key:
+install -m 0755 -d /etc/apt/keyrings;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
+chmod a+r /etc/apt/keyrings/docker.gpg;
+
+# ## Use the following command to set up the repository:
+echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-# Update the apt package index
-sudo apt update;
+  tee /etc/apt/sources.list.d/docker.list > /dev/null;
+# ## Update the apt package index
+apt update;
 
-# Install Docker Engine, containerd, and Docker Compose.
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y;
+# ## Install Docker Engine, containerd, and Docker Compose.
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y;
 
