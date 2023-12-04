8847d8a22bcc1dbfa9fc7d47950530db3efcb8e5
Dat Vu <vutiendat3601@spring.datvu.tech>
2023-12-04 16:04:47 +0700

    fix: auto confirm yes for apt install commands

--- docker_install-debian.sh
+++ docker_install-debian.sh
@@ -2,7 +2,7 @@
 
 # ## Set up Docker's Apt repository
 apt update;
-apt install ca-certificates curl gnupg;
+apt install ca-certificates curl gnupg -y;
 install -m 0755 -d /etc/apt/keyrings;
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg;
@@ -13,5 +13,5 @@ echo \
 apt update;
 
 # ## Install the Docker packages
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y;
 
