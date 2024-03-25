89e513e35397ad0bfc6bb33ced1352cb6dd9f868
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2024-03-26 00:38:02 +0700

    Create eclipise-temurin_install-debian.sh

---
+++ eclipise-temurin_install-debian.sh
@@ -0,0 +1,5 @@
+apt install -y wget apt-transport-https gpg
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+apt update
+apt install temurin-17-jre -y
