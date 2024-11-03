3bd644740fb3840b00820b57884bbdbb3998e12a
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-11-03 22:26:39 +0700

    add execute permission for *.sh files

--- goword/uninstall.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-rd /s /q "database" "storage" "goword-api" "goword-fe"
-del "docker-compose.yml"
--- goword/uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -fr database docker-compose.yml storage goword-api goword-fe
